1a187dabeed9f808d621cd6aed67b3d514f05c52
i_don.t_care90@mail.ru <SSqt21iSEaTq>
2017-04-15 17:33:50 +0300

    доделать дома

---
+++ ClassWork/Task10/src/ru/itpark/MainOutputStream.java
@@ -0,0 +1,20 @@
+package ru.itpark;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainOutputStream {
+    public static void main(String[] args) throws IOException {
+        OutputStream outputStream = new FileOutputStream("Figures.txt");
+
+        String FiguresMessage = "square, circle";
+
+        byte FigureMessage[] = FiguresMessage.getBytes();
+        try {
+            outputStream.write(FigureMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
